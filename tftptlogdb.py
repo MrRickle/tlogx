b63199850486d9a9efd01c56bb8ea0877d19e770
MrRickle <rick@hallocks.us>
2014-12-09 18:31:49 -0600

    better naming, pre-copy file so we don't have the file open for a long time.

--- tftptlogdb.py
+++ tftptlogdb.py
@@ -5,6 +5,7 @@ from ftplib import FTP
 import logging
 import datetime
 import os
+import shutil
 
 def getSize(fileobject):
     fileobject.seek(0,2) # move the cursor to the end of the file
@@ -20,13 +21,16 @@ logger.debug('starting program')
 spath = u'./'
 filepath = u'./data/'
 filename = u'tlog.db'
+destprefix = u'home'
 ftpsite = "ftp.hallocks.us"
 user = "tlogxdata@hallocks.us"
 password = "rlhTiiX@7"
 nowstring = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
 parts = filename.split('.')
-destfilename = parts[0]+nowstring+'.'+parts[1]
-logger.debug('starting ftp from {0} to {1}'.format(filepath+filename, user+'@'+ftpsite+' '+spath))
+destfilename = destprefix+parts[0]+nowstring+'.'+parts[1]
+logger.debug('copying {0} to {1}'.format(filepath+filename,destfilename))
+shutil.copy(filepath+filename,destfilename)
+logger.debug('starting ftp from {0} to {1}'.format(destfilename, user+'@'+ftpsite+' '+spath))
 ftp = ftplib.FTP(ftpsite)
 logger.debug('logging in')
 ftp.login(user, password)
@@ -36,6 +40,7 @@ logger.debug ('directory changed to '+spath)
 starttime = datetime.datetime.strptime(str(datetime.datetime.now()),u"%Y-%m-%d %H:%M:%S.%f")
 myfile = open(filepath+filename, 'rb')
 filesize = getSize(myfile)
+logger.debug ('filesize is {0}'.format(filesize))
 ftp.storbinary('STOR '+destfilename, myfile)
 myfile.close()
 endtime = datetime.datetime.strptime(str(datetime.datetime.now()),u"%Y-%m-%d %H:%M:%S.%f")
