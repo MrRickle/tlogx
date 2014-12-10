e453001c413b3f4b08a20ffe8fcb4dcba4369aea
MrRickle <rick@hallocks.us>
2014-12-09 19:20:28 -0600

    hopefully it retires 10 times if it is getting ftp timeouts

--- tftptlogdb.py
+++ tftptlogdb.py
@@ -6,6 +6,7 @@ import logging
 import datetime
 import os
 import shutil
+import time
 
 def getSize(fileobject):
     fileobject.seek(0,2) # move the cursor to the end of the file
@@ -31,21 +32,31 @@ destfilename = destprefix+parts[0]+nowstring+'.'+parts[1]
 logger.debug('copying {0} to {1}'.format(filepath+filename,destfilename))
 shutil.copy(filepath+filename,destfilename)
 logger.debug('starting ftp from {0} to {1}'.format(destfilename, user+'@'+ftpsite+' '+spath))
-ftp = ftplib.FTP(ftpsite)
-logger.debug('logging in')
-ftp.login(user, password)
-logger.debug ('logged in')
-ftp.cwd(spath)  #directory to store file in
-logger.debug ('directory changed to '+spath)
-starttime = datetime.datetime.strptime(str(datetime.datetime.now()),u"%Y-%m-%d %H:%M:%S.%f")
-myfile = open(filepath+filename, 'rb')
-filesize = getSize(myfile)
-logger.debug ('filesize is {0}'.format(filesize))
-ftp.storbinary('STOR '+destfilename, myfile)
-myfile.close()
-endtime = datetime.datetime.strptime(str(datetime.datetime.now()),u"%Y-%m-%d %H:%M:%S.%f")
-copytime = endtime - starttime
-seconds = copytime.total_seconds()
-logger.debug('It took {0} sec to copy {1} bytes from {2} to {3}'.format(seconds, filesize, filepath+filename, user+'@'+ftpsite+' '+spath+'/'+destfilename))
-
-
+copied = False
+i=0
+while not copied and i<10:   #try a maximum of 10 times
+    try:
+        with ftplib.FTP(ftpsite) as ftp:
+            logger.debug('logging in')
+            ftp.login(user, password)
+            logger.debug ('logged in')
+            ftp.cwd(spath)  #directory to store file in
+            logger.debug ('directory changed to '+spath)
+            starttime = datetime.datetime.strptime(str(datetime.datetime.now()),u"%Y-%m-%d %H:%M:%S.%f")
+            with open(destfilename, 'rb') as myfile:
+                filesize = getSize(myfile)
+                logger.debug ('filesize is {0}'.format(filesize))
+                ftp.storbinary('STOR '+destfilename, myfile)
+            os.remove(destfilename)
+            endtime = datetime.datetime.strptime(str(datetime.datetime.now()),u"%Y-%m-%d %H:%M:%S.%f")
+            copytime = endtime - starttime
+            seconds = copytime.total_seconds()
+            logger.debug('It took {0} sec to copy {1} bytes from {2} to {3}'.format(seconds, filesize, filepath+filename, user+'@'+ftpsite+' '+spath+'/'+destfilename))
+            copied = True;
+    except ftplib.all_errors as e:
+        logger.warn('Ftp fail -> '+ str(e))
+        print( 'Ftp fail -> ', e )
+        time.sleep(600)
+    i=i+1
+if i>=10:
+    logger.critical('**** Unable to copy, gave up after 10 tries  ****')
